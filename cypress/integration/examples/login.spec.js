e15f2820446197f20344d256d254a585b07584f1
ShotaTogawa <s.togawa0112@gmail.com>
2020-03-11 11:15:56 -0700

    wrote test case

---
+++ cypress/integration/examples/login.spec.js
@@ -0,0 +1,17 @@
+describe('working with inputs', () => {
+	it('should fill username', () => {
+		// TODO
+	});
+
+	it('should fill password', () => {
+		// TODO
+	});
+
+	it('should submit login form', () => {
+		// TODO
+	});
+
+	it('should display error message', () => {
+		// TODO
+	});
+});
